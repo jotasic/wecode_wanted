5ddba2bd6c6429c8d85df512d472be7a32d8d3c0
TaewooKim <embedded61@gmail.com>
2021-10-20 11:00:20 +0900

    Add: modeling 구현 - accounts, posts app 생성 - 각 model 작성

---
+++ posts/models.py
@@ -0,0 +1,14 @@
+from django.db   import models
+from django.conf import settings
+
+
+class Post(models.Model):
+    author     = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True)
+    title      = models.CharField(max_length=255)
+    content    = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    edited_at  = models.DateTimeField(auto_now=True)
+    is_active  = models.BooleanField(default=True)
+
+    class Meta:
+        db_table = 'posts'
